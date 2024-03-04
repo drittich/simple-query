78d4ef7368f1b812b3155e7d3026bea6b2ca71db
D'Arcy Rittich <drittich@gmail.com>
2024-03-04 10:28:13 -0500

    initial commit

---
+++ SimpleQuery/DbContextEntity.cs
@@ -0,0 +1,21 @@
+﻿namespace Linq3Sql
+{
+	public class DbContextEntity
+	{
+		internal DbContextService? _dbContext = null;
+		internal ReferenceFetchMode _fetchReferencesType = ReferenceFetchMode.None;
+		internal ICollection<string>? _entitiesToFetch = null;
+
+		internal ReferenceFetchMode GetChildrenReferenceFetchMode()
+		{
+			return _fetchReferencesType == ReferenceFetchMode.SingleLevel ? ReferenceFetchMode.None : _fetchReferencesType;
+		}
+
+		internal bool GetFetchReferences(string entityName)
+		{
+			return _fetchReferencesType == ReferenceFetchMode.SingleLevel
+				|| _fetchReferencesType == ReferenceFetchMode.Recursive
+				|| (_fetchReferencesType == ReferenceFetchMode.ByName && _entitiesToFetch is not null && _entitiesToFetch.Contains(entityName));
+		}
+	}
+}
---
+++ SimpleQuery/ReferenceFetchMode.cs
@@ -0,0 +1,11 @@
+﻿namespace Linq3Sql
+{
+	public enum ReferenceFetchMode
+	{
+		None, // Indicates no references will be populated.
+		SingleLevel, // Indicates only references for the initially requested object will be populated.
+		Recursive, // Indicates all references, recursively, will be populated.
+		ByName // Indicates only specifically named references will be populated.
+	}
+
+}
